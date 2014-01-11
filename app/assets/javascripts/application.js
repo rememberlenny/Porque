a08fefe4151658a8a5e0b2a89d4db30d9cb45ad5
RememberLenny <leonard_bogdonoff@condenast.com>
2014-01-11 12:43:13 -0500

    Added checked function based on javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,11 +14,6 @@
 //= require jquery_ujs
 //= require foundation
 //= require turbolinks
-//= require underscore
-//= require backbone
-//= require backbone_rails_sync
-//= require backbone_datalink
-//= require backbone/medical_simulation
 //= require_tree .
 
 $(function(){
--- app/assets/javascripts/backbone/views/questions/show_view.js.coffee
+++
@@ -1,8 +0,0 @@
-MedicalSimulation.Views.Questions ||= {}
-
-class MedicalSimulation.Views.Questions.ShowView extends Backbone.View
-  template: JST["backbone/templates/questions/show"]
-
-  render: ->
-    @$el.html(@template(@model.toJSON() ))
-    return this
