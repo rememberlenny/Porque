cc9e858383f668f291ca40e065d4bfeb9ecab4b9
RememberLenny <leonard_bogdonoff@condenast.com>
2014-01-13 22:33:34 -0500

    Secured the edit screen for the full items

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140113033654) do
+ActiveRecord::Schema.define(version: 20140114031232) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20140113033654) do
     t.integer  "questions_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "response"
   end
 
   create_table "posts", force: true do |t|
