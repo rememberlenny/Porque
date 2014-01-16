2de597fad69d99376fc31e777bb7ebd16289fca4
RememberLenny <leonard_bogdonoff@condenast.com>
2014-01-16 01:19:51 -0500

    migrate intro

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140115031507) do
+ActiveRecord::Schema.define(version: 20140116061654) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -39,6 +39,13 @@ ActiveRecord::Schema.define(version: 20140115031507) do
     t.datetime "updated_at"
     t.text     "response"
     t.integer  "nextQuestion"
+  end
+
+  create_table "intros", force: true do |t|
+    t.text     "content"
+    t.integer  "scenario_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "posts", force: true do |t|
